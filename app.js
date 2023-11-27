e61980b305117df611938e1edb9346924142c188
maricd7 <dejanmarci259@gmail.com>
2023-11-27 13:27:17 +0100

    starting to write software

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const transactionName = document.getElementById('transaction-name'); 
+const transactionType = document.getElementById('type-value'); 
+const transactionDate = document.getElementById('date-value'); 
+const transactionAmout = document.getElementById('amount-value'); 
+const submitButton = document.getElementById('submit-transaction'); 
+
+transactionType.addEventListener('change', ()=>{
+    console.log(transactionType)
+})
