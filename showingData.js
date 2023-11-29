ec53b04d3f4b197f002556a86579580bfa0c2308
maricd7 <87302839+maricd7@users.noreply.github.com>
2023-11-29 09:13:21 +0100

    Add files via upload

--- showingData.js
+++ showingData.js
@@ -2,10 +2,16 @@ const transactions = JSON.parse(localStorage.getItem("transactionList")) || [];
 const incomeValue = document.getElementById("total-income");
 const expenseValue = document.getElementById("total-expense");
 const balanceStatus = document.getElementById("balance-status");
-const balanceContainer =  document.getElementById('balance-container') ;
+const balanceContainer = document.getElementById("balance-container");
+const transactionHistoryContainer = document.getElementById(
+  "transactions-history"
+);
 
 const incomeData = [];
 const expenseData = [];
+let firstDate, lastDate;
+const expenseTransactionNames = [];
+const incomeTransactionNames = [];
 
 function sumCalculator(array) {
   let expenseSum = 0;
@@ -14,14 +20,16 @@ function sumCalculator(array) {
   array.forEach((transaction) => {
     if (transaction.type === "Expense") {
       expenseSum += transaction.amount;
-      expenseValue.innerHTML = expenseSum;
       expenseData.push(transaction.amount);
+      expenseTransactionNames.push(transaction.name);
     } else if (transaction.type === "Income") {
       incomeSum += transaction.amount;
-      incomeValue.innerHTML = incomeSum;
       incomeData.push(transaction.amount);
+      incomeTransactionNames.push(transaction.name);
     }
   });
+  expenseValue.innerHTML = expenseSum;
+  incomeValue.innerHTML = incomeSum;
   balanceStatusCalculator(array);
 }
 
@@ -31,14 +39,20 @@ function balanceStatusCalculator(array) {
     if (value.type == "Expense") {
       balanceStatusNumber -= value.amount;
       balanceStatus.innerHTML = balanceStatusNumber;
-      balanceStatusNumber < 0 ? balanceContainer.classList.add("negative") : balanceContainer.classList.add('positive') ;
-      console.log('positive')
+      balanceStatusNumber < 0
+        ? balanceContainer.classList.add("negative")
+        : balanceContainer.classList.add("positive");
     } else {
       balanceStatusNumber += value.amount;
       balanceStatus.innerHTML = balanceStatusNumber;
-      balanceStatusNumber < 0 ? balanceContainer.classList.add("negative") : balanceContainer.classList.add('positive') ;
+      if (balanceStatusNumber < 0) {
+        balanceContainer.classList.remove("positive");
+        balanceContainer.classList.add("negative");
+      } else {
+        balanceContainer.classList.remove("negative");
+        balanceContainer.classList.add("positive");
+      }
     }
-    
   });
 }
 
@@ -49,20 +63,24 @@ const incomeChart = document.getElementById("incomeChart");
 new Chart(incomeChart, {
   type: "line",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: incomeTransactionNames,
     datasets: [
       {
         label: "Your Income",
         data: incomeData,
         borderWidth: 1,
         borderWidth: 2,
-        borderColor: "rgba(75, 192, 192, 1)",
-        backgroundColor: "rgba(75, 192, 192, 0.2)",
-        pointBackgroundColor: "rgba(75, 192, 192, 1)",
+        borderColor: "rgba(142, 245, 142, 1)",
+        backgroundColor: "rgba(142, 245, 142, 1)",
+        pointBackgroundColor: "rgba(142, 245, 142, 1)",
       },
     ],
   },
   options: {
+    animation: {
+      duration: 2000,
+      easing: "easeInOutQuart",
+    },
     scales: {
       y: {
         beginAtZero: true,
@@ -75,16 +93,23 @@ const expenseChart = document.getElementById("expenseChart");
 new Chart(expenseChart, {
   type: "line",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: expenseTransactionNames,
     datasets: [
       {
         label: "Your Expenses",
         data: expenseData,
         borderWidth: 1,
+        borderColor: "rgba(255, 0, 43, 1)",
+        backgroundColor: "rgba(255, 0, 43, 1)",
+        pointBackgroundColor: "rgba(255, 0, 43, 1)",
       },
     ],
   },
   options: {
+    animation: {
+      duration: 2000,
+      easing: "easeInOutQuart",
+    },
     scales: {
       y: {
         beginAtZero: true,
@@ -93,6 +118,28 @@ new Chart(expenseChart, {
   },
 });
 
+transactions.forEach((transaction) => {
+  let thContainer = document.createElement("div");
+  thContainer.classList.add("thcontainer");
 
-const date  = new Date() ; 
-console.log(date.getFullYear())
+  let thtdContainer = document.createElement("div");
+  thtdContainer.classList.add("thtdcontainer");
+
+  let thAmount = document.createElement("p");
+  thAmount.innerHTML = transaction.amount;
+  thAmount.classList.add("history-amount");
+
+  let thName = document.createElement("p");
+  thName.innerHTML = transaction.name;
+  thName.classList.add("history-name");
+
+  let thDate = document.createElement("p");
+  thDate.innerHTML = transaction.date;
+  thDate.classList.add("history-date");
+
+  thtdContainer.appendChild(thName);
+  thtdContainer.appendChild(thDate);
+  thContainer.appendChild(thtdContainer);
+  thContainer.appendChild(thAmount);
+  transactionHistoryContainer.appendChild(thContainer);
+});
